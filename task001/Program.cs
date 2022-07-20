932c4a7bbcbbdb15e0e6998242c4123b8a834913
«Vasiliy» <pyatin_vs@mail.ru>
2022-07-21 02:11:16 +0300

    Задача 64: Задайте значения M и N. Напишите программу, которая выведет произведение натуральных чисел в промежутке от M до N.

---
+++ task001/Program.cs
@@ -0,0 +1,29 @@
+﻿// Решение только через рекурсию!
+
+// Задача 64: Задайте значения M и N. 
+// Напишите программу, которая выведет произведение натуральных чисел в промежутке от M до N.
+
+// M = 1; N = 5. ->120
+
+// M = 4; N = 6. -> 120
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите стартовое значение M: ");
+int m = int.Parse(ReadLine());
+WriteLine("Введите конечное значение N: ");
+int n = int.Parse(ReadLine());
+
+
+WriteLine(MultiplicationMN (m, n));
+
+int MultiplicationMN (int s, int e)
+{
+    if (s <= e) return s*MultiplicationMN(s+1,e);
+    if (s == 0) return 0;
+    else return 1;
+    
+}
